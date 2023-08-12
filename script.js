147e154f43ecc015453e8ff90d1b93b08a1e51fd
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-12 10:37:32 +0200

    Capitalize Challenge

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-// Type Coersion
-let x;
+let name = 'John';
+let age = 31;
 
-x = 5 + '5';
-
-x = 5 + Number('5');
-
-x = 5 * '5';
-
-x = 5 + null;
-
-x = Number(null);
-
-x = Number(true);
-x = Number(false);
-
-x = 5 + true;
-
-x = 5 + false;
-
-x = 5 + undefined;
-
-console.log(x);
-
-
-
-console.log(x, typeof x);
+let myString = 'developer';
+let myNewString = myString.charAt(0).toUpperCase()+myString.substring(1);
+console.log(myNewString);
