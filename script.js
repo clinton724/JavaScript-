68eea63df41e513cd40365bcf4051c179e34d3a9
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-12 10:45:18 +0200

    Capitalize Challenge

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let name = 'John';
 let age = 31;
 
-let myString = 'developer';
-let myNewString = myString.charAt(0).toUpperCase()+myString.substring(1);
+let myString = 'my name is clinton letsoela';
+
+let x = myString.split(' ');
+
+let myNewString = '';
+
+for(let i = 0;i < x.length;i++) {
+    myNewString += x[i][0].toUpperCase() + x[i].substring(1) + ' ';
+}
+
 console.log(myNewString);
