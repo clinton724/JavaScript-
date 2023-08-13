6cb3b2eaaf9346b0d026afb7c3dbb638b96c3cfd
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-13 14:50:08 +0200

    For Of

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-/* Loops and high order array methods */
-// FizzBuzz Challenge
+/* For Of loop */
 
-for(let i = 1; i <= 100; i++) {
-    if(i % 3 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if(i % 3 === 0) {
-        console.log('Fizz');
-    } else if(i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
-    }
+const items = ['books','table','chair','kite','cup'];
+
+for(const item of items) {
+    console.log(item);
 }
