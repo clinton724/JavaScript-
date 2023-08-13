48fce4bddd87821a5f8c25642c3ebaca47e34084
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-13 17:21:41 +0200

    Array method challenges

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-/* Higher order array methods */
-/* Important methods: 
-   - arr.forEach(): doesn't return anything
-   - arr.filter(): returns something
-   - arr.map(): doesn't return anything
-   - arr.reduce(): returns something
- */
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// Using the reduce method to find the sum of all numbers
-
-let sum = numbers.reduce((previousValue, currentValue) => {
-    return previousValue + currentValue;
-}, 0);
-
-const cart = [
-    {id: 1, name: 'Product 1', price: 130},
-    {id: 2, name: 'Product 2', price: 150},
-    {id: 3, name: 'Product 3', price: 175},
+/* Higher order array Challenges */
+// Challenge 1
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@gmail.com',
+    phone: '111-111-1111',
+    age: 30,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Poe',
+    email: 'jane@gmail.com',
+    phone: '222-222-2222',
+    age: 25,
+  },
+  {
+    firstName: 'Bob',
+    lastName: 'Foe',
+    email: 'bob@gmail.com',
+    phone: '333-333-3333',
+    age: 45,
+  },
+  {
+    firstName: 'Sara',
+    lastName: 'Soe',
+    email: 'Sara@gmail.com',
+    phone: '444-444-4444',
+    age: 19,
+  },
+  {
+    firstName: 'Jose',
+    lastName: 'Koe',
+    email: 'jose@gmail.com',
+    phone: '555-555-5555',
+    age: 23,
+  },
 ];
 
-let sum2 = cart.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue.price;
+const younPeople = people.filter((item) => {
+    return item.age <= 25;
+}).map((element) => {
+    return {name: `${element.firstName} ${element.lastName}`, email: element.email};
+})
+
+// Challenge 2
+const numbers = [2, -38, 50, 20, -21, -9, 7];
+const sum = numbers.filter((item) => {
+    return item > 0;
+}).reduce((accumulate, currentValue) => {
+    return accumulate + currentValue;
 }, 0);
-console.log(sum2);
+
+// Challenge 3
+const words = ['coder', 'programmer', 'developer'];
+
+const capitilizedWords = words.map((item) => {
+     return item[0].toUpperCase() + item.slice(1);
+})
+console.log(capitilizedWords);
