76ec82175e38ce005c1a95d96a6c83686a052ad2
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-13 16:39:21 +0200

    Array reduce

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 /* Higher order array methods */
+/* Important methods: 
+   - arr.forEach(): doesn't return anything
+   - arr.filter(): returns something
+   - arr.map(): doesn't return anything
+   - arr.reduce(): returns something
+ */
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Double numbers using map
-let double = numbers.map(item => item * 2);
-// console.log(double); 
+// Using the reduce method to find the sum of all numbers
 
-// We can also chain array methods
-// Example, get even numbers and double them
+let sum = numbers.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+}, 0);
 
-let evenDouble = numbers.filter((number) => number % 2 === 0)
-                         .map((double) => double * 2);
+const cart = [
+    {id: 1, name: 'Product 1', price: 130},
+    {id: 2, name: 'Product 2', price: 150},
+    {id: 3, name: 'Product 3', price: 175},
+];
 
-console.log(evenDouble);
+let sum2 = cart.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.price;
+}, 0);
+console.log(sum2);
