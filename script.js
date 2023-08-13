833acecce3ac613040827d6e18f9b1964c9027a6
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-13 10:36:22 +0200

    Calculator challenge

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-function sayHello() {
-    console.log('Hello World');
-}
+/* Function Challenge */
 
-sayHello();
+// // Challenge 1
+// const getCelsius = (x) => (x - 32) * 5 / 9; 
 
-// Array Params
+// //console.log(`The temperature in Celsius is ${getCelsius(32)} \xB0C`);
 
-function getRandom(...arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length + 1);
+// // Challenge 2
+// const minMax = (rest) =>  {return {min:Math.min(...rest), max:Math.max(...rest)};}
 
-    const item = arr[randomIndex];
-    console.log(item);
-}
+// // console.log(minMax([1,2,3,4,5,6,7]));
 
-getRandom(1,2,3,4,5,6,7,8,9,10);
+// // Challenge 3
+// (function (length, width) {
+//     const area = length * width;
+//     console.log(`The area of a rectangle with a length of ${length} and a width
+//     of ${width} is ${area}`);
+// })(10, 5);
+
+/* Calculator Challenge */
+ function calculator(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = 'Invalid operation.';
+    }
+    return result;
+ }
+
+ console.log(calculator(100, 50, ';'));
