5a8840d3a73c14cd5d6d690ea9524660a3501be0
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-13 16:16:23 +0200

    Array map

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 /* Higher order array methods */
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Filter out even numbers
-let even = numbers.filter((item) => item % 2 === 0);
-console.log(even); 
+// Double numbers using map
+let double = numbers.map(item => item * 2);
+// console.log(double); 
+
+// We can also chain array methods
+// Example, get even numbers and double them
+
+let evenDouble = numbers.filter((number) => number % 2 === 0)
+                         .map((double) => double * 2);
+
+console.log(evenDouble);
