015329231c4d09373adb2ec8f6ceac9657576b41
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-15 17:42:07 +0200

    keyboard events

--- script.js
+++ script.js
@@ -1,43 +1,37 @@
-const logo = document.querySelector('img');
+/* Keyboard events */
+const itemInput = document.querySelector('#item-input');
 
-function onClick (e) {
-  // console.log(e.target);
-  // e.target.style.backgroundColor = 'black';
-  // console.log(e.type);
-  // console.log(e.timeStamp);
-  //  console.log(e.clientX);
-  //  console.log(e.clientY);
-  // console.log(e.offsetX);
-  // console.log(e.offsetY);
-  // console.log(e.pageX);
-  // console.log(e.pageY);
-  // console.log(e.screenX);
-  // console.log(e.screenY);
+const onKeyPress = (e) => {
+  console.log('Key press');
+};
+
+const onKeyUp = (e) => {
+  console.log('key up');
 }
 
-function onDrag(e) {
-  document.querySelector('h1').textContent = `X ${e.clientX} Y ${e.clientY}`;
+const onKeyDown = (e) => {
+  // key
+  // if(e.key === 'Enter') {
+  //   alert('You pressed Enter');
+  // }
+  // keyCode
+  // if(e.keyCode === 13) {
+  //   alert('You pressed enter');
+  // }
+  // code
+  if(e.code === 'Digit1') {
+    console.log('You pressed 1');
+  }
+
+  if(e.repeat) {
+    console.log('You are holding down ' + e.key);
+  }
+
+  console.log('shift: '+e.shiftKey);
+  console.log('control: '+e.ctrlKey);
+  console.log('alt: '+e.altKey);
 }
-logo.addEventListener('click', onClick);
-logo.addEventListener('drag', onDrag);
+// itemInput.addEventListener('keypress', onKeyPress); 
+// itemInput.addEventListener('keyup', onKeyUp); 
+ itemInput.addEventListener('keydown', onKeyDown); 
 
-document.querySelector('a').addEventListener('click', function (e) {
-   e.preventDefault();
-
-   console.log('clicked');
-});
-
-/*
-- `target` - The element that triggered the event
-- `currentTarget` - The element that the event listener is attached to (These are the same in this case
-- `type` - The type of event that was triggered
-- `timeStamp` - The time that the event was triggered
-- `clientX` - The x position of the mouse click relative to the window
-- `clientY` - The y position of the mouse click relative to the window
-- `offsetX` - The x position of the mouse click relative to the element
-- `offsetY` - The y position of the mouse click relative to the element
-- `pageX` - The x position of the mouse click relative to the page
-- `pageY` - The y position of the mouse click relative to the page
-- `screenX` - The x position of the mouse click relative to the screen
-- `screenY` - The y position of the mouse click relative to the screen
-*/
