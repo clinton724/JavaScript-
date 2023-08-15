a51b8a5000ec1362d333678a2067fcc064d9c2a6
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-15 15:42:14 +0200

    The event object

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const logo = document.querySelector('img');
 
-const onClick = () => console.log('click event');
-const onDoubleClick = () => {
-  if(document.body.style.backgroundColor != 'purple') {
-    document.body.style.backgroundColor = 'purple';
-    document.body.style.color = 'white';
-  } else {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black';
-  }
-};
+function onClick (e) {
+  // console.log(e.target);
+  // e.target.style.backgroundColor = 'black';
+  // console.log(e.type);
+  // console.log(e.timeStamp);
+  //  console.log(e.clientX);
+  //  console.log(e.clientY);
+  // console.log(e.offsetX);
+  // console.log(e.offsetY);
+  // console.log(e.pageX);
+  // console.log(e.pageY);
+  // console.log(e.screenX);
+  // console.log(e.screenY);
+}
 
-const onRightClick = () => console.log('Right click event');
-const onMouseDown = () => console.log('Mouse down event');
-const onMouseWheel = () => console.log('Mouse wheel event');
-const onMouseOver = () => console.log('Mouse over event');
-const onDragStart = () => console.log('Drag start event');
-const onDrag = () => console.log('Drag event');
-
-
-
-// Event Listeners
-logo.addEventListener('dblclick', onDoubleClick);
-logo.addEventListener('contextmenu', onRightClick);
-logo.addEventListener('mousedown', onMouseDown);
-logo.addEventListener('wheel', onMouseWheel);
-logo.addEventListener('mouseover', onMouseOver);
-logo.addEventListener('dragstart', onDragStart);
+function onDrag(e) {
+  document.querySelector('h1').textContent = `X ${e.clientX} Y ${e.clientY}`;
+}
+logo.addEventListener('click', onClick);
 logo.addEventListener('drag', onDrag);
 
+document.querySelector('a').addEventListener('click', function (e) {
+   e.preventDefault();
 
+   console.log('clicked');
+});
 
+/*
+- `target` - The element that triggered the event
+- `currentTarget` - The element that the event listener is attached to (These are the same in this case
+- `type` - The type of event that was triggered
+- `timeStamp` - The time that the event was triggered
+- `clientX` - The x position of the mouse click relative to the window
+- `clientY` - The y position of the mouse click relative to the window
+- `offsetX` - The x position of the mouse click relative to the element
+- `offsetY` - The y position of the mouse click relative to the element
+- `pageX` - The x position of the mouse click relative to the page
+- `pageY` - The y position of the mouse click relative to the page
+- `screenX` - The x position of the mouse click relative to the screen
+- `screenY` - The y position of the mouse click relative to the screen
+*/
