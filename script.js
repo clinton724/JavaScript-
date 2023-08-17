60e16c43ec9c851cc3032040e63ad084300b6111
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-17 10:51:53 +0200

    key code project

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-/* Keyboard events */
-const itemInput = document.querySelector('#item-input');
+// Method 1
+// window.addEventListener('keydown', (e) => {
+//   const insert = document.querySelector('#insert');
 
-const onKeyPress = (e) => {
-  console.log('Key press');
-};
+//   insert.innerHTML = `<div class="key">
+//                         ${e.key === ' ' ? 'Space' : e.key}
+//                         <small>e.key</small>
+//                       </div>
 
-const onKeyUp = (e) => {
-  console.log('key up');
-}
+//                       <div class="key">
+//                         ${e.keyCode}
+//                         <small>e.keyCode</small>
+//                       </div>
 
-const onKeyDown = (e) => {
-  // key
-  // if(e.key === 'Enter') {
-  //   alert('You pressed Enter');
-  // }
-  // keyCode
-  // if(e.keyCode === 13) {
-  //   alert('You pressed enter');
-  // }
-  // code
-  if(e.code === 'Digit1') {
-    console.log('You pressed 1');
+//                       <div class="key">
+//                         ${e.code}
+//                         <small>event.code</small>
+//                       </div>`;
+// })
+
+// Method 2
+function showKeyCodes (e) {
+  const insert = document.querySelector('#insert');
+  insert.innerHTML = ' ';
+  const keyCodes = {
+    'e.key': e.key === ' ' ? 'Space' : e.key,
+    'e.keyCode': e.keyCode,
+    'e.code': e.code
   }
 
-  if(e.repeat) {
-    console.log('You are holding down ' + e.key);
+  for(let key in keyCodes) {
+    const div = document.createElement('div');
+    div.className = 'key';
+    const small = document.createElement('small');
+    const keyText = document.createTextNode(key);
+    const keyCodeText = document.createTextNode(keyCodes[key]);
+    div.appendChild(keyCodeText);
+    small.appendChild(keyText);
+    div.appendChild(small);
+    insert.appendChild(div);
+    console.log(keyCodes[key]);
   }
-
-  console.log('shift: '+e.shiftKey);
-  console.log('control: '+e.ctrlKey);
-  console.log('alt: '+e.altKey);
 }
-// itemInput.addEventListener('keypress', onKeyPress); 
-// itemInput.addEventListener('keyup', onKeyUp); 
- itemInput.addEventListener('keydown', onKeyDown); 
- // https://www.toptal.com/developers/keycode
 
+window.addEventListener('keydown', showKeyCodes);
