0f995beeb340133b94ad4adb0a07c4c5dddf000f
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-17 11:27:15 +0200

    input events

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-// Method 1
-// window.addEventListener('keydown', (e) => {
-//   const insert = document.querySelector('#insert');
+const itemInput = document.getElementById('item-input');
+const priorityInput = document.getElementById('priority-input');
+const checkbox = document.getElementById('checkbox');
+const heading = document.querySelector('h1');
 
-//   insert.innerHTML = `<div class="key">
-//                         ${e.key === ' ' ? 'Space' : e.key}
-//                         <small>e.key</small>
-//                       </div>
-
-//                       <div class="key">
-//                         ${e.keyCode}
-//                         <small>e.keyCode</small>
-//                       </div>
-
-//                       <div class="key">
-//                         ${e.code}
-//                         <small>event.code</small>
-//                       </div>`;
-// })
-
-// Method 2
-function showKeyCodes (e) {
-  const insert = document.querySelector('#insert');
-  insert.innerHTML = ' ';
-  const keyCodes = {
-    'e.key': e.key === ' ' ? 'Space' : e.key,
-    'e.keyCode': e.keyCode,
-    'e.code': e.code
-  }
-
-  for(let key in keyCodes) {
-    const div = document.createElement('div');
-    div.className = 'key';
-    const small = document.createElement('small');
-    const keyText = document.createTextNode(key);
-    const keyCodeText = document.createTextNode(keyCodes[key]);
-    div.appendChild(keyCodeText);
-    small.appendChild(keyText);
-    div.appendChild(small);
-    insert.appendChild(div);
-    console.log(keyCodes[key]);
-  }
+function onInput (e) {
+  console.log(e.target.value);
 }
 
-window.addEventListener('keydown', showKeyCodes);
+function onCheckbox (e) {
+  const isChecked = e.target.checked;
+  heading.textContent = isChecked ? 'Checked' : 'Not Checked';
+}
+
+function onFocus (e) {
+  console.log('Input is focus');
+  itemInput.style.outlineStyle = 'solid';
+  itemInput.style.outlineWidth = '1px';
+  itemInput.style.outlineColor = 'red';
+}
+
+function onBlur (e) {
+  console.log('Input is blur');
+  itemInput.style.outlineStyle = 'none';
+}
+itemInput.addEventListener('input', onInput);
+priorityInput.addEventListener('change', onInput);
+checkbox.addEventListener('input', onCheckbox);
+
+itemInput.addEventListener('focus', onFocus);
+itemInput.addEventListener('blur', onBlur);
+
+
+
+
+
